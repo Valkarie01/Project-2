4236ed20b69767b4ba36bbe0c37aaeea9334b901
sophia01 <sophiajnavarro1501@gmail.com>
2022-07-22 08:07:37 -0500

    changed main page to /

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(
 
 app.use('/artist', artistRoutes)
 app.use('/users', userRoutes)
-app.use('/genre', genreRoutes)
+app.use('/', genreRoutes)
 
 // ======= PORT ========
 const PORT = process.env.PORT
